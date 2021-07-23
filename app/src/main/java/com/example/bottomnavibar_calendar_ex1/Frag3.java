6148b8074ae4c2435832a696c8a2236e15240310
LYJ <yjlee9909@gmail.com>
2021-07-23 23:27:47 +0900

    bottom_navibar

---
+++ app/src/main/java/com/example/bottomnavibar_calendar_ex1/Frag3.java
@@ -0,0 +1,26 @@
+package com.example.bottomnavibar_calendar_ex1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag3 extends Fragment {
+
+    private View view;
+
+
+    @Nullable
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag3, container, false);
+
+
+        return view;
+    }
+}
